d5811004bc5cebbdf1c56d0b8900dd009ab1a47f
Suha Siddiqui <suhasid@my.yorku.ca>
2022-06-15 18:18:24 -0400

    dropdown menu for services

---
+++ dropdown/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div id="app">
+  <header>
+    <Navbar />
+  </header>
+</div>
+</template>
+
+<script>
+import Navbar from './components/Navbar';
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  },
+};
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: 'montserrat', sans-serif;
+}
+
+header{
+  width: 100vw;
+  background-color: #222;
+  padding: 15px;
+
+}
+</style>
---
+++ dropdown/src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav>
+    <Dropdown title="Services" :items="services" />
+  </nav>
+</template>
+
+<script>
+import Dropdown from './Dropdown';
+export default {
+  name: 'Navbar',
+  components: {
+    Dropdown
+  },
+  data () {
+    return {
+      services: [
+        {
+          title: 'Web',
+          link: '#'
+        },
+        {
+          title: 'Design',
+          link:'#'
+        },
+        {
+          title: 'Videos',
+          link: '#'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+nav .menu-item {
+  color: #FFF;
+  padding: 10px 20px;
+  position: relative;
+  text-align: center;
+  border-bottom: 3px solid transparent;
+  display: flex;
+  transition: 0.4s;
+}
+nav .menu-item.active,
+nav .menu-item:hover {
+  background-color: #444;
+  border-bottom-color: #FF5858;
+}
+nav .menu-item a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
